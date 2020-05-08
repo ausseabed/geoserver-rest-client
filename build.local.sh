5713973b13618e4ded916ecb0d105cc3a2144234
DaveP <david.penton@ga.gov.au>
2020-05-08 11:15:51 +0000

    Changing coverage file handling

--- build.local.sh
+++ build.local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd modules
 # Tested using swagger-codegen-cli-3.0.19
-for file in `cat ../list_of_modules.txt`
+for file in `cat ../list_of_modules.txt | grep datastores`
 do
 	rootname=`echo $file | sed "s/\..*//"` 
 	echo $rootname
